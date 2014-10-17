c04e2da22e6303f803a87c10da71c2f6d52eb91b
johnreaganmoore <johnreaganmoore@gmail.com>
2014-10-16 21:09:11 -0400

    Finished Basic Stormy

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,17 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var precipitationLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
+    
+    
+    private let apiKey = "dce4f93e2019ff0ca0acbc63b83ba563"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
     }
+    
+    func getCurrentWeatherData() -> Void {
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+            
+            if (error == nil) {
+                let dataObject = NSData(contentsOfURL: location)
+                let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
+                
+                let currentWeather = Current(weatherDictionary: weatherDictionary)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.temperatureLabel.text = "\(currentWeather.temperature)"
+                    self.iconView.image = currentWeather.icon!
+                    self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
+                    self.humidityLabel.text = "\(currentWeather.humidity)"
+                    self.precipitationLabel.text = "\(currentWeather.precipProbability)"
+                    self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    //Stop animating refresh indicator
+                    
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshButton.hidden = false
+                    self.refreshActivityIndicator.hidden = true
+                })
+            } else {
+               
+                let networkIssueController = UIAlertController(title: "Error", message: "Unable to load data; connectivity error", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    //Stop animating refresh indicator
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshButton.hidden = false
+                    self.refreshActivityIndicator.hidden = true
+                })
+            }
+        })
+        
+        downloadTask.resume()
 
+    }
+    
+    @IBAction func refresh() {
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        getCurrentWeatherData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
